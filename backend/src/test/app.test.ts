d6f0b6a29491909576b435a13cb4d4adc76dff71
William Putnam <wmputnam@gmail.com>
2023-09-03 13:50:49 -0700

    added to and improved members BE tests

--- backend/src/test/app.test.ts
+++ backend/src/test/app.test.ts
@@ -1,7 +1,9 @@
 import { expect } from "chai";
 
-describe("index test", function () {
-  it("should always pass", function() {
+const fn = () => `${__filename.split('/').pop()}`;
+
+describe(`${fn()}: index test`, function () {
+  it("should always pass", function () {
     expect(true).to.equal(true);
   });
 });
--- backend/src/test/members/members.test.ts
+++ backend/src/test/members/members.test.ts
@@ -1,96 +1,287 @@
 import app from "../../app";
-// src/test/members/members.test.ts
 import supertest from "supertest";
 import { expect } from "chai";
-import shortid from "shortid";
-import mongoose from "mongoose";
 import debug from "debug";
+import { equal } from "assert";
 
 const log: debug.IDebugger = debug('app:test-members');
-let firstMemberIdTest = ''; //later to hold value returned by API
-const randomizer = Date.now()
-const firstMemberBody = {
-  email: `wmputnam+${randomizer}@gmail.com`,
-  firstName: "William",
-  lastName: "Putnam"
+
+
+const fn = () => `${__filename.split('/').pop()}`;
+function delay(time: number) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
 
-const newFirstName = "Billy";
-const newFirstName2 = "Bill";
-const newLastName = "Robertsson-Putnam";
+async function runDelay() {
+  await delay(1000);
+}
 
-describe('members endpoints', function () {
+describe(`${fn()}: members GET`, function () {
   let request: supertest.SuperAgentTest;
-  before(function () {
-    // server = app.listen(3033);
+
+  const randomizer = Date.now();
+
+  const testMemberBody = {
+    email: `wmputnam+${randomizer}@gmail.com`,
+    firstName: "William",
+    lastName: "Putnam"
+  };
+
+  let testMemberId = '';
+
+
+  before(async function () {
     request = supertest.agent(app);
+    const res = await request.post("/members").send(testMemberBody);
+    testMemberId = res.body.id;
   });
+
   after(function (done) {
-    // server.close( function() {
-    mongoose.connection.close();
-    // });
     done();
-  })
-
-  it("should allow a POST to /members", async function () {
-    const res = await request.post("/members").send(firstMemberBody);
-
-    firstMemberIdTest = res.body.id;
-    expect(res.status).to.equal(200);
-    expect(res.body).not.to.be.empty;
-    expect(res.body).to.be.an('object');
-    expect(res.body.id).to.be.a('string');
   })
 
   it("should allow a GET to /members", async function () {
     const res = await request.get("/members").send();
 
-    log(res.body);
     expect(res.status).to.equal(200);
     expect(res.body).not.to.be.empty;
     expect(res.body).to.be.an('array');
-    // expect( res.body.id ).to.be.a( 'string');
-    // firstMemberIdTest = res.body.id;
   })
 
   it(`should allow GET for a member by Id /members/:memberId`, async function () {
-    log(`testing members/${firstMemberIdTest}`);
-    const res = await request.get(`/members/${firstMemberIdTest}`).send();
+    log(`testing members/${testMemberId}`);
+    const res = await request.get(`/members/${testMemberId}`).send();
 
-    log(res.body);
-    log(typeof res.body)
     expect(res.status).to.equal(200);
     expect(res.body).not.to.be.empty;
     expect(res.body).to.be.an('object');
 
   });
 
-  it(`should allow PATCH for a member by Id /members/:memberId`, async function () {
-    log(`testing PATCH members/${firstMemberIdTest}`);
-    const patchBody = {
-      ...firstMemberBody,
-      firstName: newFirstName2
-    }
-    const res = await request.patch(`/members/${firstMemberIdTest}`).send(patchBody);
-    // request.get(`/members/${firstMemberIdTest}`).send();
+  it(`should return error from GET for a member by Id /members/:memberId when memberId is not found`, async function () {
+    const res = await request.get(`/members/duncell`).send();
 
-    log(res.body);
-    log(typeof res.body)
-    expect(res.status).to.equal(204);
-    expect(res.body).to.be.empty;
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.equal('Member duncell not found');
     expect(res.body).to.be.an('object');
 
+  });
+});
+
+describe(`${fn()}: member POST`, function () {
+
+  let request: supertest.SuperAgentTest;
+
+  runDelay();
+
+  const randomizer = Date.now();
+
+  const testMemberBody = {
+    email: `wmputnam+${randomizer}.post@gmail.com`,
+    firstName: "William_post",
+    lastName: "Putnam+post"
+  };
+
+
+  before(async function (done) {
+    request = supertest.agent(app);
+    done();
+  });
+
+  after(function (done) {
+    done();
+  })
+
+  it("should allow a POST to /members", async function () {
+    const res = await request.post("/members").send(testMemberBody);
+
+    expect(res.status).to.equal(200);
+    expect(res.body).not.to.be.empty;
+    expect(res.body).to.be.an('object');
+    expect(res.body.id).to.be.a('string');
+  });
+
+  it(`should return error from POST when firstName is not provided`, async function () {
+    const res = await request.post(`/members`).send({ lastName: testMemberBody.lastName });
+    console.log(res.body.error);
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- firstName');
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(2);
+    expect(res.body).to.be.an('object');
+
+  });
+
+  it(`should return error from POST when firstName is empty`, async function () {
+    const res = await request.post(`/members`).send({ ...testMemberBody, firstName: '' });
+    console.log(res.body.error);
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- firstName');
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(1);
+    expect(res.body).to.be.an('object');
+
+  });
+
+  it(`should return error from POST when lastName is not provided`, async function () {
+    const res = await request.post(`/members`).send({ firstName: testMemberBody.firstName });
+    console.log(res.body.error); // TODO FIX controller
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- lastName');
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(2);
+
+    expect(res.body.error).to.equal('Invalid value -- lastName');
+    expect(res.body).to.be.an('object');
+  });
+
+  it(`should return error from POST when lastName is empty`, async function () {
+    const res = await request.post(`/members`).send({ ...testMemberBody, lastName: '' });
+    console.log(res.body.error);
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- lastName');
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(1);
+    expect(res.body).to.be.an('object');
+
+  });
+
+  it(`should return error from POST when neither firstname nor lastName provided`, async function () {
+    const res = await request.post(`/members`).send({ email: 'test@test.it' });
+    console.log(res.body.error); // TODO FIX controller
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- firstName');
+    expect(res.body.error).to.contain('Invalid value -- lastName');
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(2);
+    expect(res.body).to.be.an('object');
+  });
+
+});
+
+describe(`${fn()}: member PUT`, function () {
+
+  let request: supertest.SuperAgentTest;
+
+  let testMemberId = '';
+
+  runDelay();
+  const randomizer = Date.now();
+
+  const testMemberBody = {
+    email: `wmputnam+${randomizer}.put@gmail.com`,
+    firstName: "William.put",
+    lastName: "Putnam.put"
+  };
+
+  const newFirstName = "Billy";
+
+  const newLastName = "Robertsson-Putnam";
+
+  before(async function () {
+    request = supertest.agent(app);
+    const res = await request.post("/members").send(testMemberBody);
+    testMemberId = res.body.id;
+  });
+
+  after(function (done) {
+    done();
   });
 
   it(`should allow PUT for a member by Id /members/:memberId`, async function () {
-    log(`testing PUT members/${firstMemberIdTest}`);
+    log(`testing PUT members/${testMemberId}`);
     const patchBody = {
-      ...firstMemberBody,
+      ...testMemberBody,
       firstName: newFirstName,
       lastName: newLastName
     }
-    const res = await request.put(`/members/${firstMemberIdTest}`).send(patchBody);
-    // request.get(`/members/${firstMemberIdTest}`).send();
+    const res = await request.put(`/members/${testMemberId}`).send(patchBody);
+    expect(res.status).to.equal(204);
+    expect(res.body).to.be.empty;
+    expect(res.body).to.be.an('object');
+  });
+
+  it(`should return error PUT for a member by Id /members/:memberId when not presenst`, async function () {
+    log(`testing PUT members/${testMemberId}`);
+    const patchBody = {
+      ...testMemberBody,
+      firstName: newFirstName,
+      lastName: newLastName
+    }
+    const res = await request.put(`/members/putcell`).send(patchBody);
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.contain('Member putcell not found');
+    expect(res.body).to.be.an('object');
+  });
+
+  it(`should return error PUT when sending an empty firstName`, async function () {
+    log(`testing PUT members/${testMemberId}`);
+    const patchBody = {
+      ...testMemberBody,
+      firstName: '',
+      lastName: newLastName
+    }
+    const res = await request.put(`/members/${testMemberId}`).send(patchBody);
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- firstName');// TODO FIX
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(1);
+    expect(res.body).to.be.an('object');
+  });
+
+  it(`should return error PUT when sending an empty lastName`, async function () {
+    log(`testing PUT members/${testMemberId}`);
+    const patchBody = {
+      ...testMemberBody,
+      firstName: newFirstName,
+      lastName: ''
+    }
+    const res = await request.put(`/members/${testMemberId}`).send(patchBody);
+    expect(res.status).to.equal(400);
+    expect(res.body.error).to.contain('Invalid value -- lastName'); // TODO FIX
+    expect(res.body.error).to.be.an('array');
+    expect(res.body.error.length).to.equal(1);
+    expect(res.body).to.be.an('object');
+  });
+
+});
+
+describe(`${fn()}: member PATCH`, function () {
+
+  let request: supertest.SuperAgentTest;
+
+  let testMemberId = '';
+
+  runDelay();
+
+  const randomizer = Date.now();
+
+  const testMemberBody = {
+    email: `wmputnam+${randomizer}.patch@gmail.com`,
+    firstName: "William.patch",
+    lastName: "Putnam.patch"
+  };
+
+  const newFirstName2 = "Bill";
+
+  before(async function () {
+    request = supertest.agent(app);
+    const res = await request.post("/members").send(testMemberBody);
+    testMemberId = res.body.id;
+
+  });
+
+  after(function (done) {
+    done();
+  });
+
+  it(`should allow PATCH for a member by Id /members/:memberId`, async function () {
+    log(`testing PATCH members/${testMemberId}`);
+    const patchBody = {
+      ...testMemberBody,
+      firstName: newFirstName2
+    }
+    const res = await request.patch(`/members/${testMemberId}`).send(patchBody);
 
     log(res.body);
     log(typeof res.body)
@@ -99,5 +290,63 @@ describe('members endpoints', function () {
     expect(res.body).to.be.an('object');
 
   });
+
+  it(`should reurn error from PATCH member not found`, async function () {
+    log(`testing PATCH members/${testMemberId}`);
+    const patchBody = {
+      ...testMemberBody,
+      firstName: newFirstName2
+    }
+    const res = await request.patch(`/members/patchcell`).send(patchBody);
+
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.contain('Member patchcell not found');
+    expect(res.body).to.be.an('object');
+
+  });
+});
+
+describe(`${fn()}: member DELETE`, function () {
+
+  let request: supertest.SuperAgentTest;
+
+  let testMemberId = '';
+
+  runDelay();
+
+  const randomizer = Date.now();
+
+  const testMemberBody = {
+    email: `wmputnam+${randomizer}.del@gmail.com`,
+    firstName: "William_del",
+    lastName: "Putnam_del"
+  };
+
+  before(async function () {
+    request = supertest.agent(app);
+    const res = await request.post("/members").send(testMemberBody);
+    testMemberId = res.body.id;
+
+  });
+
+  after(function (done) {
+    done();
+  })
+
+  it("should allow DELETE of existing member", async function () {
+    const res = await request.delete(`/members/${testMemberId}`);
+    expect(res.status).to.equal(204);
+    expect(res.body).to.be.empty;
+    expect(res.body).to.be.an('object');
+  });
+
+  it("should return error for DELETE of non-existing member", async function () {
+    const res = await request.delete(`/members/dorf`);
+    const expectedRes = { error: 'Member dorf not found' }
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.equal(expectedRes.error);
+    expect(res.body).to.be.an('object');
+  });
+
 
 });
